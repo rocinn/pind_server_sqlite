e509fa53486fbd8588c525d34e5d006713dfa3cf
yrp <rocinn.pc>
2024-09-21 15:04:34 +0800

    时间改回显示秒，修正连续保存报错问题

--- pind_server_sqlite/Common/SqliteHelper.cs
+++ pind_server_sqlite/Common/SqliteHelper.cs
@@ -556,8 +556,8 @@ select last_insert_rowid();";
             foreach (DataRow dr in dt.Rows)
             {
                 dr["htmlcontent"] = dr["htmlcontent"].ToString();
-                dr["iTimeShow"] = DateTime.Parse(dr["iTime"].ToString()).ToString("yyyy-MM-dd HH:mm");
-                dr["uTimeShow"] = DateTime.Parse(dr["uTime"].ToString()).ToString("yyyy-MM-dd HH:mm");
+                dr["iTimeShow"] = DateTime.Parse(dr["iTime"].ToString()).ToString("yyyy-MM-dd HH:mm:ss");
+                dr["uTimeShow"] = DateTime.Parse(dr["uTime"].ToString()).ToString("yyyy-MM-dd HH:mm:ss");
             }
 
             return dt;
